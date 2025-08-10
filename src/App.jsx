2e7e33378d55abe8e484c93af4b3fbc0f2510535
Gabriela <gabriela@gmail.com>
2025-08-10 16:03:27 +0300

    Added a popup modal to show a note

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
-import NoteForm from './components/NoteForm';
-import NoteItem from './components/NoteItem';
+import NoteForm from './components/NoteForm'
+import NoteItem from './components/NoteItem'
 import './App.css'
+import { Modal } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
 
 function App() {
   const [notes, setNotes] = useState([])
+  const [opened, { open, close }] = useDisclosure(false);
+  const [selectedNote, setSelectedNote] = useState(null);
 
   const addNote = (text, title) => {
     const newNote = {id: Date.now(), title, text, date: new Date(), }
@@ -17,16 +21,39 @@ function App() {
     }
   }
 
+  const handleNoteClick = (note) => {
+    setSelectedNote(note)
+    open()
+  }
+
   return (
     <div className="app-container">
       <h1>QuickNotes</h1>
       <NoteForm addNote={addNote} />
       <div className="notes-grid">
         {notes.map((note) => (
-          <NoteItem key={note.id} note={note} onDelete= { () => deleteNote(note.id)}/>
+          <NoteItem key={note.id} note={note} onDelete= { () => deleteNote(note.id)} onNoteClick={() => handleNoteClick(note)}/>
         ))}
       </div>
+
+       <Modal 
+          opened={opened} 
+          onClose={close} 
+          size="lg"
+          centered
+          className= "modal"
+        >
+        {selectedNote && (
+          <div>
+            <p>{selectedNote.title}</p>
+            <p>{selectedNote.text}</p>
+            <small>Created: {new Date(selectedNote.date).toLocaleString()}</small>
+          </div>
+        )}
+      </Modal>
     </div>
+
+    
   )
 }
 
--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function NoteItem({ note , onDelete}) {
+function NoteItem({ note , onDelete, onNoteClick}) {
   const { title, text, date } = note
   const formattedDate = new Date(date).toLocaleString()
 
   return (
-    <div className="note">
+    <div className="note"  onClick={onNoteClick}>
       {title && <h3>{title}</h3>} 
       <p>{text}</p>
       <div className= "note-footer">
